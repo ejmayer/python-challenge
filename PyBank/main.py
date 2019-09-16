8c03a1290c2824a983b894837bccfc7a505ad849
Emayer <53119577+jeneric1023@users.noreply.github.com>
2019-09-16 11:02:22 -0500

    first commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,45 @@
 #This is main.py in PyBank
+
+import os
+import csv
+
+Budget_Data_path = os.path.join("Budget_Data.csv")
+
+
+with open(Budget_Data_path, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    print(csvreader)
+
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        print(row)
+        
+
+    date = int(row[1])
+    profits_losses = int(row[2])
+
+
+    print("Financial Analysis_____________________")
+
+    months = 0
+
+    for row in csv_reader
+    months += date
+
+    print("Total Number of Months: "+ months)
+
+    Total_Profits += profits_losses
+
+    print("Total Profits: " + Total_Profits)
+    
+    Average = Total_Profits / months
+
+    print("Average Change: " + Average)
+
+    MaxProfit, MinProfit = [],[]
+    
+    for row in csv_reader:
+        MaxProfit.append
+
